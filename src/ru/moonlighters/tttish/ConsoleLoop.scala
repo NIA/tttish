c918a7fdc9b1f854301673c666ef243aedcbb2d6
Ivan Novikov <nia.afti@gmail.com>
2012-12-23 20:53:50 +0400

    Initial commit
    Includes simple interactive console loop

---
+++ src/ru/moonlighters/tttish/ConsoleLoop.scala
@@ -0,0 +1,21 @@
+package ru.moonlighters.tttish
+
+object ConsoleLoop {
+  def run() {
+    Iterator continually(prompt) map(_.trim) filterNot(_.isEmpty) foreach(
+      _ match {
+        case "help" => println(helpText)
+        case "exit" => return
+        case x => println("Unknown command: '%s'. Seriously, type help if not sure." format x)
+      }
+    )
+  }
+
+  def prompt:String = {
+    Console.print(promptText)
+    Console.readLine()
+  }
+
+  val promptText = "ttt > "
+  val helpText = "Commands list:\nhelp\t- that is, print this text\nexit\t- nuff said, quits the program"
+}
---
+++ src/ru/moonlighters/tttish/Main.scala
@@ -0,0 +1,10 @@
+package ru.moonlighters.tttish
+
+object Main extends App {
+  println("TTTish: The Time Tracker interactive shell v.0.0")
+  println("Type help/exit for... you know")
+
+  ConsoleLoop run()
+
+  println("Exiting. Goodbye!")
+}
