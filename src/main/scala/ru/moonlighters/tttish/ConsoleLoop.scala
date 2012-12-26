f02fb036bfc31be5ca2bcea3a30510460b873681
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2012-12-27 00:48:30 +0700

    More flexible ConsoleLoop

--- src/main/scala/ru/moonlighters/tttish/ConsoleLoop.scala
+++ src/main/scala/ru/moonlighters/tttish/ConsoleLoop.scala
@@ -3,39 +3,67 @@ package ru.moonlighters.tttish
 import collection.immutable.ListMap
 
 object ConsoleLoop {
+  private object LoopEnd extends Error
+
   def run() {
-    Iterator continually(prompt) map { _.trim } filterNot {_.isEmpty} foreach { input =>
-      // Allow writing only beginning of command, e.g. "up" for "updates", "h" for "help" etc.
-      commands.keys.toList filter { _.startsWith(input) } match {
-        case Seq(cmd) => cmd match {
-          case "help" => println(helpText)
-          case "exit" => return
-          case "updates" => UpdatesController.showUpdates()
-          case x => println("Sorry, command '%s' is not yet implemented :(" format input)
+    try {
+      Iterator continually(prompt) map { _.trim } filterNot {_.isEmpty} foreach { input =>
+        val (cmdLike, args) = input.split("\\s+", 2) match {
+          case Array(x) => (x, null)
+          case Array(x, y) => (x, y)
         }
-        case Seq() =>
-          println("Unknown command: '%s'. Seriously, type help if not sure." format input)
-        case Seq(cmds @ _*) =>
-          val candidates = cmds.mkString("'", "', '", "'")
-          println("Ambiguous command: '%s'. Possible candidates: %s.".format(input, candidates))
+        // Allows writing only beginning of command, e.g. "up" for "updates", "h" for "help" etc.
+        commands.keys.toSeq filter { _.startsWith(cmdLike) } match {
+          case Seq(cmd) =>
+            (commands.get(cmd) map { _.func } getOrElse { _: String =>
+              println("Sorry, command '%s' is not yet implemented :(" format input)
+            })(args)
+          case Seq() =>
+            println("Unknown command: '%s'. Seriously, type help if not sure." format input)
+          case Seq(cmds @ _*) =>
+            val candidates = cmds.mkString("'", "', '", "'")
+            println("Ambiguous command: '%s'. Possible candidates: %s.".format(input, candidates))
+        }
       }
+    } catch {
+      case LoopEnd => return
     }
   }
 
-  def prompt: String = {
+  /** Shell command.
+    *
+    * @param description command description
+    * @param func command implementation that can receive some arguments
+    */
+  private case class Command(description: String, func: (String) => Unit)
+
+  // TODO: move commands to the right place
+  private val commands: Map[String, Command] = ListMap(
+    "updates" -> Command("show the last updates", { _ =>
+      UpdatesController.showUpdates()
+    }),
+    "post" -> Command("post new update", { msg =>
+      // TODO: implement me
+      assert(msg != null && msg.nonEmpty)
+      println("... hard work of posting update with message \"%s\"..." format msg)
+    }),
+    "help" -> Command("that is, print this text", { _ =>
+      println(helpText)
+    }),
+    "exit" -> Command("nuff said, quit the program", { _ =>
+      throw LoopEnd
+    })
+  )
+
+  private val promptText = "ttt > "
+
+  private def prompt: String = {
     print(promptText)
     readLine()
   }
 
-  val promptText = "ttt > "
-
-  val commands = Map(
-    "updates" -> "show the last updates",
-    "help"    -> "that is, print this text",
-    "exit"    -> "nuff said, quit the program"
-  )
-  val helpTabStop = 8
-  val helpText = "Commands list:\n" + commands.map({
+  private val helpTabStop = 8
+  private val helpText = "Commands list:\n" + commands.map({
     case (cmd, desc) => cmd + " "*(helpTabStop - cmd.length) + "- " + desc
   }).mkString("\n")
 }
